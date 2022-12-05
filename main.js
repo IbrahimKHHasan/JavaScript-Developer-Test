739d56a3a91c107995f09a170ca43ab34dd5d285
Ibrahim Hasan <abr0130615@fet.ju.edu.jo>
2022-12-05 22:57:54 +0300

    rendering data

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// fetching JSON data
 jsonURL =
   'https://filltext.com/?rows=10&fname={firstName}&lname={lastName}&category=["category1","category2","category3"]&pretty=true';
 fetch(jsonURL)
@@ -9,7 +10,33 @@ fetch(jsonURL)
   })
   .then((jsonData) => {
     var data = jsonData;
-    console.log(data);
+    var dataDiv = document.getElementById("data");
+    data.map((ele) => {
+      var p1 = document.createElement("p");
+      var p2 = document.createElement("p");
+      var hr = document.createElement("hr");
+      p1.innerText = ele["fname"] + " " + ele["lname"];
+      p2.innerText = ele["category"];
+      switch (ele["category"]) {
+        case "category1":
+          p2.style.color = "red";
+          break;
+
+        case "category2":
+          p2.style.color = "green";
+          break;
+
+        case "category3":
+          p2.style.color = "blue";
+          break;
+
+        default:
+          break;
+      }
+      dataDiv.appendChild(p1);
+      dataDiv.appendChild(p2);
+      dataDiv.appendChild(hr);
+    });
   })
   .catch((err) => {
     console.log(err);
